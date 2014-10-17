959ead475bcb67a2961d926bd789615bfda4381a
L.x <feexon@foxmail.com>
2014-10-17 15:14:54 +0800

    resort the methods in test

--- src/test/java/SumTest.java
+++ src/test/java/SumTest.java
@@ -7,6 +7,14 @@ import static org.junit.Assert.fail;
  * Created by L.x on 14-10-17.
  */
 public class SumTest {
+    private int sum(int[] numbers) {
+        int sum = 0;
+        for (int each : numbers) {
+            sum += each;
+        }
+        return sum;
+    }
+
     @Test
     public void sumEmptyArray() throws Exception {
         assertEquals(0, sum(new int[0]));
@@ -22,14 +30,5 @@ public class SumTest {
     public void sumTwoNumbers() throws Exception {
         assertEquals(3, sum(new int[]{1, 2}));
         assertEquals(5, sum(new int[]{2, 3}));
-
-    }
-
-    private int sum(int[] numbers) {
-        int sum = 0;
-        for (int each : numbers) {
-            sum += each;
-        }
-        return sum;
     }
 }
