f6e22758828cf0524b07da8b6b3c77c51fd63b18
L.x <feexon@foxmail.com>
2014-10-17 14:55:15 +0800

    write a touch test

---
+++ src/test/java/SumTest.java
@@ -0,0 +1,13 @@
+import org.junit.Test;
+
+import static org.junit.Assert.fail;
+
+/**
+ * Created by L.x on 14-10-17.
+ */
+public class SumTest {
+    @Test
+    public void nothing() throws Exception {
+        fail();
+    }
+}
