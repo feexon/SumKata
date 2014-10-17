f8042e503f386a7aae5502eeeaa57d9cc110e88b
L.x <feexon@foxmail.com>
2014-10-17 15:13:31 +0800

    introduce a variable 'each' prepared for foreach statement

--- src/test/java/SumTest.java
+++ src/test/java/SumTest.java
@@ -28,7 +28,8 @@ public class SumTest {
     private int sum(int[] numbers) {
         int sum = 0;
         for (int i = 0; i < numbers.length; i++) {
-            sum += numbers[i];
+            int each = numbers[i];
+            sum += each;
         }
         return sum;
     }
